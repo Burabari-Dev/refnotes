7de4c7d2bbf0b5a5e8be4e6dbca1bc9ac6177477
Burabari <burabaridev@gmail.com>
2023-02-11 15:17:46 +0100

    add UserBadge to HeaderRow

--- src/features/header-row/HeaderRow.js
+++ src/features/header-row/HeaderRow.js
@@ -1,10 +1,12 @@
 import Logo from '../logo/Logo';
+import UserBadge from '../user-badge/UserBadge';
 import styles from './HeaderRow.module.css';
 
 export default function HeaderRow() {
   return (
     <div className={styles.container}>
       <Logo />
+      <UserBadge />
     </div>
   )
 }
