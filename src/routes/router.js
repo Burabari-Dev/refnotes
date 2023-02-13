2086d6541f6b7ea4cbff39e98afd1ec7278e554e
Burabari <burabaridev@gmail.com>
2023-02-13 08:22:09 +0100

    add Technologies sub-route to Home page

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom';
 import ErrorPage from '../pages/Error/error-page';
 import Home from '../pages/Home/home';
+import Technologies from '../pages/Technologies/technologies';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Home/>,
     errorElement: <ErrorPage/>,
-
+    children: [
+      {
+        path: '/',
+        element: <Technologies />,
+        errorElement: <ErrorPage/>
+      }
+    ]
   }
 ])
