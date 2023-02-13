d80e1c1e6b3d8702d87168ac216443ce0ddbb106
Burabari <burabaridev@gmail.com>
2023-02-13 08:21:43 +0100

    add outlet for sub-page routing in Home page

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,10 +1,12 @@
 import HeaderRow from '../../features/header-row/HeaderRow';
+import { Outlet } from 'react-router-dom';
 import styles from './home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.main}>
       <HeaderRow />
+      <Outlet />
     </div>
   )
 }
