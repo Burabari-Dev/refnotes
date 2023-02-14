595c6b715f14e54e0596c6cae27c3ccc815f81b0
Burabari <burabaridev@gmail.com>
2023-02-14 05:38:06 +0100

    create Tech-Detail page

---
+++ src/pages/Tech-Detail/tech-detail.jsx
@@ -0,0 +1,8 @@
+import styles from './tech-detail.module.css';
+
+export default function TechDetail({ detailUrl }) {
+  //TODO: logic to get details of this page from detailUrl
+  return(
+    <div>Technology Detail Page</div>
+  )
+}
