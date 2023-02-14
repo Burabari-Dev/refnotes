ae1d478a1339cffba332b1aeb942a03ac983f105
Burabari <burabaridev@gmail.com>
2023-02-14 06:34:38 +0100

    remove entry for ordering because arrays are always ordered

--- src/test-data/techs.js
+++ src/test-data/techs.js
@@ -2,32 +2,26 @@ export const techs = [
   {
     id: 't0001',
     name: 'Cloud Computing (AWS)',
-    moduleGroups: ['mg001', 'mg002'] //mg002
   },
   {
     id: 't0002',
     name: 'Python Programming',
-    moduleGroups: ['mg003', 'mg004', 'mg005', 'mg006'] //mg014
   },
   {
     id: 't0003',
     name: 'Vagrant',
-    moduleGroups: ['mg015', 'mg016', 'mg017', 'mg018'] //mg018
   },
   {
     id: 't0004',
     name: 'Linux & Bash Scripting',
-    moduleGroups: ['mg019', 'mg020'] //mg020
   },
   {
     id: 't0005',
     name: 'Terraform',
-    moduleGroups: ['mg021', 'mg022', 'mg023', 'mg024'] //mg026
   },
   {
     id: 't0006',
     name: 'Docker',
     icon: './icons/docker.png',
-    moduleGroups: ['mg021', 'mg022', 'mg023', 'mg024'] //mg026
   },
 ]
