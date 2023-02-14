695d495d10b7f743b8b0238fefe40f7701c7745f
Burabari <burabaridev@gmail.com>
2023-02-14 05:37:06 +0100

    add navigation to UserBadge

--- src/features/user-badge/UserBadge.js
+++ src/features/user-badge/UserBadge.js
@@ -1,9 +1,12 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './UserBadge.module.css';
 
 export default function UserBadge({ user={} }) {
+  const navigate = useNavigate();
 
   function handleSignIn() {
     //TODO: Sign In logic
+    navigate('/user/007')
   }
 
   function handleUserClick() {
