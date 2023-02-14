b606b2a663982b71797979373fed8d98532abd5f
Burabari <burabaridev@gmail.com>
2023-02-14 06:35:23 +0100

    add functions for other required data

--- src/services/backend.js
+++ src/services/backend.js
@@ -1,5 +1,8 @@
-import { moduleGroups } from '../test-data/module-groups';
 import { techs } from '../test-data/techs';
+import { moduleGroups } from '../test-data/module-groups';
+import { modules } from '../test-data/modules';
+import { paragraphs } from '../test-data/paragraphs';
+import { contents } from '../test-data/contents';
 
 const isDevEnv = process.env.NODE_ENV === 'development';
 
@@ -13,6 +16,27 @@ export function getAllTechs() {
 export function getTechModuleGroups(techId) {
   if (isDevEnv){
     return moduleGroups.filter(mg => (mg.techId === techId));
+  }
+  //TODO: logic to fetch data from backend
+}
+
+export function getModules(moduleGroupId) {
+  if (isDevEnv){
+    return modules.filter(m => (m.groupId === moduleGroupId));
+  }
+  //TODO: logic to fetch data from backend
+}
+
+export function getParagraphs(moduleId) {
+  if (isDevEnv){
+    return paragraphs.filter(p => (p.moduleId === moduleId));
+  }
+  //TODO: logic to fetch data from backend
+}
+
+export function getContents(paragraphId) {
+  if (isDevEnv){
+    return contents.filter(c => (c.paragraphId === paragraphId));
   }
   //TODO: logic to fetch data from backend
 }
