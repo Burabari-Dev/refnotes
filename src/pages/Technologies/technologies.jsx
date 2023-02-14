05878dc739921954915d0d9d090ca8c487642ef1
Burabari <burabaridev@gmail.com>
2023-02-14 06:36:20 +0100

    minor formating

--- src/pages/Technologies/technologies.jsx
+++ src/pages/Technologies/technologies.jsx
@@ -4,6 +4,7 @@ import styles from './technologies.module.css';
 
 export default function Technologies() {
   const techs = getAllTechs();
+  
   return (
     <div className={styles.content}>
       {techs.map(t => <TechBadge title={t.name} key={t.id} />)}
