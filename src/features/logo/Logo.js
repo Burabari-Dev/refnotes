287860a1b08366c8afce41c9fec323f28e5dd257
Burabari <burabaridev@gmail.com>
2023-02-14 05:35:38 +0100

    add navigation to Logo component

--- src/features/logo/Logo.js
+++ src/features/logo/Logo.js
@@ -1,7 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './Logo.module.css';
 
 export default function Logo() {
+  const navigate = useNavigate();
+  
+  function handleNavigate(){
+    navigate('/');
+  }
+
   return (
-    <div className={styles.logo}>Ref-Notes</div>
+    <div className={styles.logo} onClick={handleNavigate} > Ref-Notes </div>
   )
 }
