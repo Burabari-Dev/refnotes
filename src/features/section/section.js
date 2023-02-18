3017bf11edcd19ff9be10544397923042b84bea3
Burabari <burabaridev@gmail.com>
2023-02-18 04:34:25 +0100

    add hadling of empty content to Section component

--- src/features/section/section.js
+++ src/features/section/section.js
@@ -2,14 +2,18 @@ import { useState } from 'react';
 import MultiContent from '../multi-content/multiContent';
 import styles from './section.module.css';
 
-export default function Section({ type, contents=[] }) {
+export default function Section({ contents = [] }) {
   const [activeIndex, setActiveIndex] = useState(0);
-  
-  if(contents.length < 2) {
+
+  if (contents.length === 0) {
+    return <></>
+  }
+
+  if (contents.length < 2) {
     return <p>{contents[0].data}</p>
   }
 
-  const multiContents = contents.map(c => <MultiContent key={c.id} type={type} data={c.data} />);
+  const multiContents = contents.map(c => <MultiContent key={c.id} type={c.type} data={c.data} />);
 
   function handleClick(index) {
     setActiveIndex(index);
