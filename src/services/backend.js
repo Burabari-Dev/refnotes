f6a3b34b907ab1db5cc88d0e07410531135f7949
Burabari <burabaridev@gmail.com>
2023-02-18 04:35:16 +0100

    re-desing the demo backend

--- src/services/backend.js
+++ src/services/backend.js
@@ -6,120 +6,76 @@ import { contents } from '../test-data/contents';
 
 const isDevEnv = process.env.NODE_ENV === 'development';
 
+function getModule(moduleId) {
+  return modules.find(m => m.id === moduleId);
+}
+
+function getParagraphs(moduleId) {
+  return paragraphs.filter(p => p.moduleId === moduleId);
+}
+
 export function getAllTechs() {
   if (isDevEnv) {
     return techs;
   }
   //TODO: logic to fetch data from backend
+  return [];
 }
 
-export function getTechModuleGroups(techId) {
+export function getTechTitle(techId) {
   if (isDevEnv) {
-    return moduleGroups.filter(mg => (mg.techId === techId));
+    return techs.find(t => t.id === techId).name;
   }
   //TODO: logic to fetch data from backend
+  return '';
 }
 
-export function getModules(moduleGroupId) {
+export function getTechById(techId) {
   if (isDevEnv) {
-    return modules.filter(m => (m.groupId === moduleGroupId));
+    return techs.find(t => t.id === techId);
   }
   //TODO: logic to fetch data from backend
+  return {};
 }
 
-export function getParagraphs(moduleId) {
+
+export function getTechModuleMenus(techId) {
   if (isDevEnv) {
-    return paragraphs.filter(p => (p.moduleId === moduleId));
-  }
-  //TODO: logic to fetch data from backend
-}
-
-export function getContents(paragraphId) {
-  if (isDevEnv) {
-    return contents.filter(c => (c.paragraphId === paragraphId));
-  }
-  //TODO: logic to fetch data from backend
-}
-
-function getTechTitleById(techId){
-  if(isDevEnv){
-    return getAllTechs().find(at => at.id === techId)?.name;
-  }
-}
-
-// ################################################ //
-
-/*
-Structure of the DATA:
-{
-  title: string,                -> ModuleGroup Title
-  values: [                     -> 
-    {
-      title: string,            -> Module Title
-      values: [                 -> 
-        {
-          id: string,           -> Paragraph ID
-          content: [content]    -> Array of Paragraph Contents
-        }
-      ]
-    }
-  ]
-}
-*/
-export function getTechDetailsData(techId) {
-  if (isDevEnv) {
-    // const tech = techs.find(t => t.id === techId);
-    const moduleGroups = getTechModuleGroups(techId);
-    const data = moduleGroups.map(mg => (
-      {
-        title: mg.title, values: (
-          getModules(mg.id).map(m => (
-            {
-              title: m.title, values: (
-                getParagraphs(m.id).map(p => (
-                  { id: p.id, contents: getContents(p.id) }
-                ))
-              )
-            }
-          ))
-        )
-      }
-
-    ))
-
-    return data;
-  }
-}
-
-
-/*
-STRUCTURE OF THE DATA
-{
-  moduleGroups: [ {}, {}, {} ],
-  modules:      [ {}, {}, {} ],
-  contents:     [ [{}], [{}], [{}] ]
-}
- */
-export function getTechDetailsData2(techId) {
-  if(isDevEnv){
-    const moduleGroups = getTechModuleGroups(techId);
-    const modules = getModules(moduleGroups[0]?.id);
-
-    const menus = [];
-
-    moduleGroups.forEach(mg => {
-      menus.push(mg);
-      getModules(mg?.id).forEach(m => menus.push(m))
+    let arr = [];
+    const groups = moduleGroups.filter(mg => mg.techId === techId);
+    groups.forEach(g => {
+      arr.push(g);
+      const mods = modules.filter(m => m.groupId === g.id);
+      arr.push(...mods);
     })
 
-    const paragraphs = getParagraphs(modules[0]?.id);
-    const contents = paragraphs.map(p => ([getContents(p?.id)])) // => Array of array of contents because some contents can have multiple versions
-    return {
-      techTitle: getTechTitleById(techId),
-      menus: menus,
-      moduleGroups: moduleGroups, 
-      modules: modules,
-      contents: contents
-    }
+    return arr;
   }
+  //TODO: logic to fetch data from backend
+  return [];
+}
+
+export function getModuleData(moduleId) {
+  if (isDevEnv) {
+    const sections = getParagraphs(moduleId);
+
+    return sections.map(s => (
+      {
+        paragraphId: s.id,
+        contents: contents.filter(c => c.paragraphId === s.id)
+      })
+    )
+
+  }
+  //TODO: logic to fetch data from backend
+  return [];
+}
+
+export function getFirstModuleData(techId) {
+  if (isDevEnv) {
+    const firstModule = getTechModuleMenus(techId).find(mm => mm.id.startsWith('m0'));  //TODO: Change this atrocity for prod code
+    return getModuleData(firstModule?.id);
+  }
+  //TODO: logic to fetch data from backend
+  return [];
 }
