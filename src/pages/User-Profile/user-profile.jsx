71caef7bcd618fa08ab95e1e1428013a32965d9c
Burabari <burabaridev@gmail.com>
2023-02-18 04:39:15 +0100

    add dummy style to placate VS Code

--- src/pages/User-Profile/user-profile.jsx
+++ src/pages/User-Profile/user-profile.jsx
@@ -2,6 +2,6 @@ import styles from './user-profile.module.css';
 
 export default function UserProfile(){
   return(
-    <div>This is the User Profile page</div>
+    <div className={styles.none}>This is the User Profile page</div>
   )
 }
