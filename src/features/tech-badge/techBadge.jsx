748ad347cb71cc7b59da6cfd8f69af0b636a12f5
Burabari <burabaridev@gmail.com>
2023-02-27 03:06:27 +0100

    update TechBadge to use id, title props

--- src/features/tech-badge/techBadge.jsx
+++ src/features/tech-badge/techBadge.jsx
@@ -1,14 +1,14 @@
 import { useNavigate } from 'react-router-dom';
 import styles from './techBadge.module.css';
 
-export default function TechBadge({ tech }) {
+export default function TechBadge({ id, title }) {
   const navigate = useNavigate();
 
   function handleNavigation() {
-    navigate(`/tech/${tech.id}`);
+    navigate(`/tech/${id}`);
   }
 
   return (
-    <div className={styles.wrapper} onClick={handleNavigation}>{tech.title}</div>
+    <div className={styles.wrapper} onClick={handleNavigation}>{title}</div>
   )
 }
