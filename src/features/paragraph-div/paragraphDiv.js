972108fed96db3b18a6477c7de287d68fa2b8e37
Burabari <burabaridev@gmail.com>
2023-02-27 03:05:35 +0100

    delete comment

--- src/features/paragraph-div/paragraphDiv.js
+++ src/features/paragraph-div/paragraphDiv.js
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import MultiContent from '../multi-content/multiContent';
 import styles from './paragraphDiv.module.css';
 
-// export default function ParagraphDiv({ contents = [] }) {
 export default function ParagraphDiv({ contents = [] }) {
   const [activeIndex, setActiveIndex] = useState(0);
 
